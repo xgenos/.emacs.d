44df1560f033c1c0f3b36ecd9e44e74d0fb2eaab
Biswakalyan Bhuyan <biswakalyan890@gmail.com>
2022-06-07 17:16:27 +0530

    some plugins

---
+++ elpa/lsp-mode-20220606.702/lsp-mode-pkg.el
@@ -0,0 +1,18 @@
+(define-package "lsp-mode" "20220606.702" "LSP mode"
+  '((emacs "26.1")
+    (dash "2.18.0")
+    (f "0.20.0")
+    (ht "2.3")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0"))
+  :commit "ae5ef019291a95443f0d9e97bad85f612e6a8e76" :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :keywords
+  '("languages")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
